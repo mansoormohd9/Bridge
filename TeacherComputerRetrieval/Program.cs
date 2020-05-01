439ea8546cdd3a51d78a185c75b60b73e0dd9ce2
Shaik Mansoor Mohammad <skmansoormohd@gmail.com>
2020-05-01 23:51:21 +0530

    Setting up initial selection model

--- TeacherComputerRetrieval/Program.cs
+++ TeacherComputerRetrieval/Program.cs
@@ -6,7 +6,26 @@ namespace TeacherComputerRetrieval
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"Please make an appropriate selection from below to continue
+                            1.Distance along certain routes.
+                            2.Number of different routes between two academies.
+                            3.Shortest route between two academies.");
+            
+            var userSelection = Console.ReadLine();
+            switch(userSelection){
+                case "1":
+                    //do
+                    break;
+                case "2":
+                    //do
+                    break;
+                case "3":
+                    //do
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Please try again.");
+                    break;
+            }
         }
     }
 }
